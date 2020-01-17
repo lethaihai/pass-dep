f9ff2d6b8bd536098e900fb57129aef8acc32c9d
hailt <hailt@rikkeisoft.com>
2020-01-17 14:11:10 +0700

    config to deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :user, "root"
+set :deploy_via, :remote_cache
+set :conditionally_migrate, true
+set :rails_env, "production"
+
+# Phần IP thì bạn thay thế cho phù hợp với IP của Docker container nhé
+server "172.17.0.2", user: fetch(:user), port: fetch(:port), roles: %w(web app db)
